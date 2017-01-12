0fce1ca823d754928c85581c817ee15590fc0428
李涛 <137908979>
2017-01-12 20:28:23 +0800

    微信小程序

---
+++ pages/logs/logs.js
@@ -0,0 +1,14 @@
+// pages/logs/logs.js
+Page({
+	data: {
+		logs: []
+	},
+	onShow: function() {
+		var logs = wx.getStorageSync('todo_logs')
+		if (logs) {
+			this.setData({
+				logs: logs.reverse()
+			})
+		}
+	},
+})
